f611753467a770e2423a367975e151f45e9f021f
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2019-11-19 14:51:49 -0500

    Add gem annotate

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Item < ApplicationRecord
 end
