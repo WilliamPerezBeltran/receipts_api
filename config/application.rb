c9ed0e2349f26453ac678c01c4d07bb99bfd9dfc
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2019-11-19 14:06:57 -0500

    add rack-cors, jwt , simple_command gem , add lib/json_web_token.rb

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,17 @@ module ReceiptsApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.autoload_paths << Rails.root.join('lib')
+
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get,:patch, :put, :delete,:post, :options]
+      end
+    end
+
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_181047) do
+ActiveRecord::Schema.define(version: 2019_11_19_185651) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.string "confirmation_token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
